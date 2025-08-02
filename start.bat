38cce640b0a3df30524c939b509fd0e0c8f4cae1
Notitoh23 <simonmulundumina@gmail.com>
2025-08-03 01:09:34 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting KickStart Tracker...
+echo.
+echo Make sure VS Code is installed and Live Server is enabled.
+echo.
+echo Opening in VS Code Live Server...
+start http://127.0.0.1:5500
+code .
+pause
